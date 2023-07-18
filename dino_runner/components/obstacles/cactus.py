e53301108189599a3ddddadbaa0b16f44f4943c3
JhonathanAlejandro01 <jhonathanalejandroa01@hotmail.com>
2023-07-18 15:14:33 -0500

    Cactus class working

---
+++ dino_runner/components/obstacles/cactus.py
@@ -0,0 +1,16 @@
+"""cactus obstacle module"""
+import random
+from dino_runner.components.obstacles.obstacle import Obstacle
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
+
+
+class Cactus(Obstacle):
+    """
+    Cactus class inherits from Obstacle
+    """
+
+    def __init__(self):
+        image_list = SMALL_CACTUS + LARGE_CACTUS
+        select_image = random.choice(image_list)
+        super().__init__(select_image)
+        self.rect.y = 300
