5f5da21e093839c00b4a8640aac696290f91e4f0
JhonathanAlejandro01 <jhonathanalejandroa01@hotmail.com>
2023-07-18 15:38:20 -0500

    Implementation of dinosaur and obstacle

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,17 @@
 """Game Dinosaur"""
 import pygame
+from dino_runner.components.obstacles.obstacle_manager import ObstacleManager
 
-from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.utils.constants import (
+    BG,
+    ICON,
+    SCREEN_HEIGHT,
+    SCREEN_WIDTH,
+    TITLE,
+    FPS
+)
+from dino_runner.components.dinosaur import Dinosaur
+# pylint: disable=no-member
 
 
 class Game:
@@ -17,6 +27,8 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
+        self.obstacle_manager = ObstacleManager()
 
     def run(self):
         # Game loop: events - update - draw
@@ -33,12 +45,15 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        self.player.update(pygame.key.get_pressed())
+        self.obstacle_manager.update(self)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
+        self.obstacle_manager.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
